9d7a18c6daa4cf8896b2bc815bc55cacd5397fc4
Constantkavaya <79209924+Constantkavaya@users.noreply.github.com>
2021-05-24 20:06:30 +0300

    Add files via upload

---
+++ dazzling_trug1.ino
@@ -0,0 +1,15 @@
+int piezobuzzer=5;
+
+
+void setup(){
+  pinMode(piezobuzzer,OUTPUT);
+  
+}
+void loop(){
+ // tone(piezobuzzer,1000,5000);
+  //delay(2000);
+ // noTone(piezobuzzer,1000);
+  //delay(2000);
+
+
+}
